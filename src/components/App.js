45c6601a89d71fd85132a6bbbea9cfae2d90fa5f
Yicai Weng <yicaiweng@gmail.com>
2020-01-22 16:03:56 -0600

    integrated weather api

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import SearchBox from './searchBox';
 import './weather.css';
-import geocoding from '../api/geocoding';
+// import geocoding from '../api/geocoding';
+import axios from 'axios';
 
 class App extends Component {
-  state = {city: {}};
+  state = {city: [], forecast: []};
 
-  onSearchSubmit = async term =>{
-    const response = await geocoding
-      .get('maps/api/geocode/json', {
-        params: {
+  onSearchSubmit = term =>{
+    axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+    params: {
           address: term,
           key: process.env.REACT_APP_GEOCODING_API
         },
+      }).then( response => {
+        this.setState({city: response.data.results[0]});
+        console.log(this.state.city)
+        let lat = this.state.city.geometry.location.lat;
+        let lon = this.state.city.geometry.location.lng;
+        // https://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key=API_KEY
+
+        axios.get('https://api.weatherbit.io/v2.0/forecast/daily', {
+          params: {
+            lat,
+            lon,
+            key: process.env.REACT_APP_WEATHER_API
+          }
+        }).then(res => {
+          this.setState({forecast: res.data})
+          console.log(this.state.forecast);
+        })
+        .catch(err => {
+          console.log(err);
+        })
       })
-        this.setState({city: response.data.results});
-        console.log(this.state);
-        console.log(this.state.city[0].geometry.location);
+      .catch(err => {
+        console.log(err);
+      })
   }
 
   render() {
