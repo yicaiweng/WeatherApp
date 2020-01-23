fa7038c0fe7d0d656fc4a1dde7a97840a248052c
Vantonio Hall <vantonio.hall@ibm.com>
2020-01-22 22:47:10 -0500

    Updates to server

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const port = process.env.PORT
 
 // Routes can be defined here
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello! Thw weather server is up and running')
+})
+
+app.get('/getWeather', (req, res) => {
+    // TODO
+    // Do any necessary input validation
+    // Call Weatherbit API
+    // Return weather data in the expected form
 })
 
 app.listen(port, console.log(`Server started on port ${port}...`))
