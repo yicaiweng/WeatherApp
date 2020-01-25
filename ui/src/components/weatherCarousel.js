b0b46222ad13a8fffa345d2f17fc3e84234e44fe
Vantonio Hall <vantonio.hall@ibm.com>
2020-01-25 15:52:03 -0600

    Expand card when selected

--- ui/src/components/weatherCarousel.js
+++ ui/src/components/weatherCarousel.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import WeatherCard from './weatherCard';
 
-const WeatherCarousel = ({ centerIndex, forecasts}) => {
-    return forecasts.slice(centerIndex - 2, centerIndex + 3).map((forecast, i) => {
-        return <WeatherCard forecasts={forecast} key={i} />
-    })
-
+const WeatherCarousel = ({ centerIndex, forecasts, selectedIndex, selectCard }) => {
+    return (
+        <div className="ui grid centered">
+            {forecasts.slice(centerIndex - 2, centerIndex + 3).map((forecast, i) => {
+                return <WeatherCard
+                        key={i}
+                        index={i}
+                        forecasts={forecast}
+                        selectedIndex={selectedIndex}
+                        selectCard={selectCard}
+                    />
+            })}
+        </div>
+    )
 }
 
 export default WeatherCarousel;
